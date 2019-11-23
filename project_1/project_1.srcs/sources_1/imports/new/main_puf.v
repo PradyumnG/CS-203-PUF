51de008545bceeee420dd18275e0b584af911d2e
PradyumnG <45365329+PradyumnG@users.noreply.github.com>
2019-11-23 08:17:14 +0530

    Update main_puf.v

--- project_1/project_1.srcs/sources_1/imports/new/main_puf.v
+++ project_1/project_1.srcs/sources_1/imports/new/main_puf.v
@@ -23,13 +23,13 @@
 module main_puf(
     input a,
     input[15:0] challenge,
-    output[15:0] response
+    output[31:0] response
     );
     genvar i;
     reg clk;
     reg[31:0] cnt;
     initial begin
-        clk=0;
+        clk=1;
         cnt=0;
     end
     always @(posedge a)begin
@@ -44,7 +44,7 @@ module main_puf(
         u=0;
         v=1;
     end
-    for(i=0;i<16;i=i+1)begin
-        arbiter a1(u,v,challenge,response[i]);
+    for(i=0;i<32;i=i+1)begin
+        arbiter a1(clk,clk,challenge,response[i]);
     end
 endmodule
