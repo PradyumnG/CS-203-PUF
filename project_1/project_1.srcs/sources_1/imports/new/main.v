5d5f638ae8c8eb39d12ce3e4caf8ea4a1ef364ef
PradyumnG <45365329+PradyumnG@users.noreply.github.com>
2019-11-23 08:16:44 +0530

    Update main.v

--- project_1/project_1.srcs/sources_1/imports/new/main.v
+++ project_1/project_1.srcs/sources_1/imports/new/main.v
@@ -25,7 +25,7 @@ module main(
     input[15:0] challenge,
     output[15:0] response
     );
-    //wire [31:0]out1;
-    main_puf mp1(a,challenge,response);
-    //xor_encoding xe(out1,response);
+    wire [31:0]out1;
+    main_puf mp1(a,challenge,out1);
+    xor_encoding xe(out1,response);
 endmodule
